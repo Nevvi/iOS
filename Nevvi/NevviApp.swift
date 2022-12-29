41aea782800fd38c9ec001c79463320058017d8f
Tyler Cobb <tyjcobb94@gmail.com>
2022-12-29 10:31:43 -0500

    Initial Commit

---
+++ Nevvi/NevviApp.swift
@@ -0,0 +1,17 @@
+//
+//  NevviApp.swift
+//  Nevvi
+//
+//  Created by Tyler Cobb on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct NevviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
